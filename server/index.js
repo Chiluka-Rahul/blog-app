ff587f5939f461ef7b5bf395857bb1e02329ea1c
Chiluka-Rahul <rahulchiluka2511@gmail.com>
2024-08-25 17:58:59 +0530

    .

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use("/api/comments",commentRoute)
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+    res.sendFile(path.join(process.cwd(),'client','dist','index.html'));
 })
 
 
