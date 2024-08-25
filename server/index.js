97eab29744b4f74ddcff678fca4dbfc66b7fc5fc
Chiluka-Rahul <rahulchiluka2511@gmail.com>
2024-08-25 16:48:14 +0530

    changes made in server

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
 app.use("/api/comments",commentRoute)
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, 'client','dist','index.html'));
